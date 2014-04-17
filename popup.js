fee119c7996b63924e748933407d489091118869
harry <apprentice@dbc-chicago-022.local>
2014-04-17 18:29:05 -0500

    Got the MVP working
    Nate helped us pass the selected text to the extension. That works
    
    Now, when you select text, displays the definition of the selected word in the extension.

--- popup.js
+++ popup.js
@@ -1,21 +1,18 @@
-var QUERY = 'gum';
 
 var definitionGenerator = {
 
-  searchInDictionary_: 'http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/' +
-                        encodeURIComponent(QUERY) + '?key=2d8fe204-c216-41b7-8c18-e16b699c0641',
-
-
-  // requestDefinition: function() {$.get(this.searchInDictionary_, function(resp) {
-  //   console.log(resp);
-  //   });
-  // }
 
   requestDefinition: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchInDictionary_, true);
-    req.onload = this.logConsole_;
-    req.send(null);
+    chrome.tabs.executeScript({code: "window.getSelection().toString();"},function(selectedText){
+      var query = selectedText[0];
+      var searchInDictionary_ = 'http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/' +
+                        encodeURIComponent(query) + '?key=2d8fe204-c216-41b7-8c18-e16b699c0641'
+
+      var req = new XMLHttpRequest();
+      req.open("GET", searchInDictionary_, true);
+      req.onload = this.logConsole_;
+      req.send(null);
+    }.bind(this));
   },
 
   logConsole_: function(e) {
@@ -23,17 +20,17 @@ var definitionGenerator = {
     xmlDef = $.parseXML( resp ),
     $xml = $( xmlDef ),
     $def = $xml.find( "entry sens mc" );
-    // console.log($def[0].innerHTML);
+    $word = $xml.find( "entry hw" )
     var def = document.createElement('p');
+    var wordName = document.createElement('h3');
     def.innerHTML = $def[0].innerHTML;
-    document.body.appendChild(def)
+    wordName.innerHTML = $word[0].innerHTML;
+    document.body.appendChild(wordName);
+    document.body.appendChild(def);
   }
 
 };
 
-
-
-// Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
   definitionGenerator.requestDefinition();
 });
